2622ee7eef9b3f6a6f867c5c264566720151d27a
Roy Li <roygagali@gmail.com>
2020-07-07 15:39:33 -0700

    Implemented gradient descent w/ matrix operations, deleted unused .py files
    - renamed train to GD, implemented GD
    - added+implemented a backpropagation method
    - added+implemented a testing method

--- CNN.py
+++ CNN.py
@@ -25,7 +25,11 @@ class NN:
 
         w_n a_m represents a weight connecting the the nth weight from the l-1th layer to the mth neuron
         """
-        # set up neuron activation and bias list
+        # vectorizing activation function and its derivative
+        self.act_f = np.vectorize(sigmoid)
+        self.act_f_prime = np.vectorize(sigmoid_prime)
+
+        # set up neuron activation and random bias list
         self.activations = [np.array([0 for i in range(0, 784)])]
         self.biases = []
         for l in hidden_layers:
@@ -34,7 +38,7 @@ class NN:
         self.activations.append(np.array([0 for i in range(0, 10)]))
         self.biases.append(np.array([np.random.random_sample()*2 - 1 for i in range(0, 10)]))
 
-        # setting up neuron weight matrices
+        # setting up neuron weight matrices w/ random weights
         self.weights = []
         for i in range(1, len(self.activations)):
             mat = []
@@ -43,14 +47,17 @@ class NN:
                 mat.append(row)
             self.weights.append(np.array(mat))
 
-        # number to NN output conversion dict
+        # setting up sum array with dummy values
+        self.sum_arr = [1 for i in self.activations]
+
+        # set up number to NN output conversion dict
         tmp = [0 for i in range(0, 9)]
         self.label_array = {0: tmp[:], 1: tmp[:], 2: tmp[:], 3: tmp[:], 4: tmp[:], 5: tmp[:], 6: tmp[:], 7: tmp[:],
                        8: tmp[:], 9: tmp[:]}
         for i in self.label_array:
             self.label_array[i].insert(i, 1)
 
-    def GD(self, learning_rate = 0.01, file_dir="training_data"):
+    def GD(self, learning_rate=1.2, file_dir="training_data"):
         """Performs one gradient descent step on the NN given an image and label list.
         learning rate is a positive float representing how 'far' a step should be taken
 
@@ -61,17 +68,18 @@ class NN:
         data = MNIST(file_dir)
         images, labels = data.load_training()
 
-        d = 0
+        c = 0
+
         for i in range(0, len(images)):
             b = self.classify(images[i])
+            tmp_c = (b - self.label_array[labels[i]])
+            tmp_c = (tmp_c.dot(tmp_c))**2
+            c += tmp_c
+            self.backprop(self.label_array[labels[i]], learning_rate)
 
-            c = (b - self.label_array[labels[i]])
-            c = (c.dot(c))**2
-            d += 1
+            print(tmp_c)
 
-            print(c)
-
-        print (str(d) + " operations")
+        print("training completed, average cost: " + str(c/60000))
 
     def classify(self, img):
         """
@@ -81,25 +89,67 @@ class NN:
         if len(img) != 784:
             return None
 
-        # set input layer to img
-        self.activations.pop(0)
-        self.activations.insert(0, np.array([i/255 for i in img]))
+        arr_img = np.array(img)
 
-        # update activations
+        # set input layer to img, update sum array at first layer
+        self.activations.pop(0)
+        self.activations.insert(0, arr_img/255)
+
+        self.sum_arr.pop(0)
+        self.sum_arr.insert(0, arr_img)
+
+        # update activations and sums for each layer
         for layer in range(1, len(self.activations)):
             self.activations.pop(layer)
             w = self.weights[layer - 1]
             a = self.activations[layer - 1]
 
-            new_act = np.array([sigmoid(i) for i in (w.dot(a) + self.biases[layer-1])])
-            self.activations.insert(layer, new_act)
+            # calculate weighted sum of current layer
+            l_sum = w.dot(a) + self.biases[layer-1]
+            self.sum_arr.pop(layer)
+            self.sum_arr.insert(layer, l_sum)
 
-        # return np.where(self.activations[-1] == max(self.activations[-1]))[0][0]
+            # apply sigmoid to all sums and add it to the current activation matrix
+            self.activations.insert(layer, self.act_f(l_sum))
+
+        # return np.where(self.activations[-1] == max(self.activations[-1]))[0][0] (returning max num code)
         return self.activations[-1]
 
-    def split_batch(self, lst, size):
+    def __split_batch(self, lst, size):
         """splits <lst> into a list containing numpy arrays with length of at least <size>
         and returns the new list"""
+
+    def backprop(self, lbl, LR):
+        """backpropagates the error of the NN when compared to a numpy array representing the expected output, lbl.
+        updates all weights and biases of the NN for some learning rate, LR. label must correspond to the most
+        recent image fed into the NN"""
+
+        # getting error of output layer (hadamar product of grad_aC and sig_prime)
+        # find partial derivative vector of cost WRT each activation(grad_aC), and sigmoid derivative vector (sig_prime)
+        grad_aC = self.activations[-1] - lbl
+        sig_prime = self.act_f_prime(self.sum_arr[-1])
+        output_err = grad_aC * sig_prime
+        err_list = [output_err]
+
+        # backpropagating the error, find error of hidden layers
+        for i in range(1, len(self.weights)):
+            err = np.dot(np.transpose(self.weights[-i]), err_list[0]) * self.act_f_prime(self.sum_arr[-i - 1])
+            err_list.insert(0, err)
+
+        # update weights and biases given from error list
+        for e in range(0, len(err_list)):
+            # updating weights
+            err_mat = np.array([err_list[e] for i in range(0, len(self.activations[e]))])
+            act_mat = np.transpose(np.array([self.activations[e] for i in range(0, len(err_list[e]))]))
+            w_grad = act_mat * err_mat
+            old_w = self.weights.pop(e)
+            new_w = np.transpose(np.transpose(old_w) - LR*w_grad)
+            self.weights.insert(e, new_w)
+
+            # updating biases
+            old_b = self.biases.pop(e)
+            new_b = old_b + LR*err_list[e]
+            self.biases.insert(e, new_b)
 
     def load(self):
         """loads weights and biases from a text file"""
@@ -109,6 +159,19 @@ class NN:
         """saves weights and biases to a text file"""
         pass
 
+    def test(self, file_dir="training_data"):
+        """tests the neural network, prints how much percentage it is correct"""
+        print("loading testing data")
+        test_data = MNIST(file_dir)
+        img, lbl = test_data.load_testing()
+
+        correct = 0
+        for i in range(0, len(img)):
+            b = self.classify(img[i])
+            if (np.where(self.activations[-1] == max(self.activations[-1]))[0][0]) == lbl[i]:
+                correct += 1
+
+        print(str(correct / len(img)) + " % accuracy")
 
 def sigmoid(x):
     """activation function for the network"""
@@ -120,7 +183,9 @@ def sigmoid_prime(x):
     return sigmoid(x) * (1 - sigmoid(x))
 
 
-testNN = NN([10,4])
+testNN = NN([10])
 testNN.GD()
+
+testNN.test()
 
 b = 12
